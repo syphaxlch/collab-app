2543ba9a72e317bf776cc148cf8c1dab59b11aff
Syphax LAKHDARCHAOUCHE <81050260+syphaxlch@users.noreply.github.com>
2025-05-14 00:09:14 -0400

    initial commit

---
+++ collab-api/src/main/java/com/syphax/collab_api/CollabApiApplication.java
@@ -0,0 +1,13 @@
+package com.syphax.collab_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabApiApplication.class, args);
+	}
+
+}
---
+++ collab-api/src/test/java/com/syphax/collab_api/CollabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syphax.collab_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
